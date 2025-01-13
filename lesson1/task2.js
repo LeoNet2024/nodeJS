8608a87911cd037a02048ca8b30a39023f880d66
Leon <162373739+LeoNet2024@users.noreply.github.com>
2025-01-13 10:43:48 +0200

    Update task2.js

--- lesson1/task2.js
+++ lesson1/task2.js
@@ -1,9 +1,15 @@
-//leon gitelman
+//leon gitelman 
+/**
+This function check if the num is prime
+**/
 function isPrimeNum(num) {
   for (let i = 2; i < num; i++) if (num % i === 0) return false;
   return true;
 }
 
-for (let i = 2; i < 237; i++) {
-  isPrimeNum(i) && console.log(i);
+// main function
+function main(){
+  for (let i = 2; i < 237; i++) {
+    isPrimeNum(i) && console.log(i);
+  }  
 }
